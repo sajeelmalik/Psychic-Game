6a359236e4fab0870b782e16836d839e9f36577e
James Harden <sajeel23@berkeley.edu>
2018-08-01 18:43:44 -0700

    fix game.js conditional arguments to suit goals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 var alphabet = "abcdefghijklmnopqrstuwxyz";
 var wins = 0;
 var losses = 0;
-var maxGuesses = 7;
+var guessesLeft = 7;
 var guesses = [];
 
 //start the game upon pressing any letter key
@@ -12,34 +12,51 @@ document.onkeyup = function(event){
     if(alphabet.includes(input)){
 
         //have the computer randomly guess any letter in the alphabet
-        var computerGuess = function(){
-            return alphabet[(Math.floor(Math.random() * alphabet.length))];
+        var computerGuess = function(str){
+            return str[(Math.floor(Math.random() * str.length))];
         };
+        var computerLetter = computerGuess(alphabet);
 
         // Set the inner HTML contents of the #score div to our updated wins, losses, and guesses
         var scoreUpdate = function(){
-            var score = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p> Your guesses: " + guesses + "</p>";
+            var score = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p> Guesses Left: " + guessesLeft + "<p> Your guesses: " + guesses + "</p>";
             document.querySelector("#score").innerHTML = score;
         };
+
+        //restarts the game upon a winning or losing condition
+        var restart = function(){
+            guessesLeft = 7;
+            guesses = [];
+            computerLetter = computerGuess(alphabet);
+        }
 
         
         
         document.onkeyup = function(event){
             guess = event.key.toLowerCase();
             guesses.push(guess);  
+            // guesses.join(" ");
+            guessesLeft--;
 
             //minor test case to view the console's interpretation of computerGuess
-            console.log(computerGuess());
+            console.log(computerLetter);
 
-            if(guess === computerGuess()){
+            if(guess === computerLetter){
                 wins++;
                 scoreUpdate();
-                computerGuess();
+                restart();
+
             }
-            else{
+            else if(guessesLeft === 0){
+                console.log("You're a fraud!");
                 losses++;
                 scoreUpdate();
-                computerGuess();
+                alert("The computer's guess was " + computerLetter + "!");
+                restart();
+                
+            }
+            else{
+                scoreUpdate();
             }
         };
         
